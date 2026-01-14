0fbed9aa117fd4933b32703a174ba95866acfc75
Karthik <azlab11_cbe@WWLx393208.onmicrosoft.com>
2026-01-14 11:47:36 +0530

    Initial Terraform code

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+}
+
+variable "vnet_name" {
+  description = "Virtual Network name"
+  type        = string
+}
+
+variable "address_space" {
+  description = "VNet address space"
+  type        = list(string)
+}
